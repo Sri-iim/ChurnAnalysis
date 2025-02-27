fd6b8f425e2ea8ade0e81090248a207063961a92
Sri-iim <embadta24012@iimsirmaur.ac.in>
2025-02-27 23:42:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, classification_report
 
 # Load the dataset
